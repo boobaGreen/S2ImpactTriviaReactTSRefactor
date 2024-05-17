7cb59ad92f06e742c46f02d4d5a20d0bc756dde1
boobaGreen <devboobagreen@gmail.com>
2024-05-18 00:25:46 +0200

    eliminato utils cn

--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-// Importing necessary libraries
-import { twMerge } from "tailwind-merge";
-import { clsx, ClassValue } from "clsx";
-
-// cn function: This function takes an array of class values, merges them into a single string using clsx,
-// and then further optimizes the class string using tailwind-merge (twMerge).
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
